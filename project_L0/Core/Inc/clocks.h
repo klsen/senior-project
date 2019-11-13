3d1f91a2cd0fa9e0c79fac74e8f0d4bf3c4046f0
Kenneth Seneres <kenneth.seneres@maine.edu>
2019-11-13 16:54:44 -0500

    new functions and code refactor for clocks file
    set rtc functions can now use args
    getters for rtc
    alarm setter written
    alarm test written
    new structs in header

--- project_L0/Core/Inc/clocks.h
+++ project_L0/Core/Inc/clocks.h
@@ -13,17 +13,52 @@
  * buttons: pb2, pb13, pb14, pb15
  */
 
-// uncomment after you get functions args sorted
-void setTime(RTC_HandleTypeDef *hrtc);
-void setDate(RTC_HandleTypeDef *hrtc);
-void setDateTime(RTC_HandleTypeDef *hrtc);
-//
-//void setAlarm();
-//
+struct times {
+	uint8_t hr;
+	uint8_t min;
+	uint8_t sec;
+};
+
+// weekday can be omitted? 0 if not set
+struct dates {
+	uint8_t yr;
+	uint8_t month;
+	uint8_t date;
+	uint8_t weekday;
+};
+
+// struct for alarm stuff
+struct alarmTimes {
+	uint8_t hr;
+	uint8_t min;
+	uint8_t sec;
+	uint8_t weekday;
+};
+
+// ---- setting rtc time ----
+void setTime(RTC_HandleTypeDef *hrtc, struct times *t);
+void setDate(RTC_HandleTypeDef *hrtc, struct dates *d);
+void setDateTime(RTC_HandleTypeDef *hrtc, struct dates *d, struct times *t);
+// ---- end of rtc setters ----
+
+// setting alarm
+void setAlarm(RTC_HandleTypeDef *hrtc, struct alarmTimes *a);
+
+// ---- getting rtc times ----
+void getTime(RTC_HandleTypeDef *hrtc, struct times *t);
+void getDate(RTC_HandleTypeDef *hrtc, struct dates *d);
+void getDateTime(RTC_HandleTypeDef *hrtc, struct dates *d, struct times *t);
+// ---- end of getters ----
+
+// ---- printing stuff ----
 void printTime(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi);
 void printDate(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi);
 void printDateTime(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi);
+// ---- end of prints ----
 
+// ---- tests ----
 void clockTest(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi);
+void alarmTest(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi);
+// ---- end of tests ----
 
 #endif
--- project_L0/Core/Src/clocks.c
+++ project_L0/Core/Src/clocks.c
@@ -8,20 +8,14 @@
 
 #include "clocks.h"
 
-// ---- functions used ---- (move to header file)
-// set_clock();
-// set_timer();
-// set_alarm();
-
-
-// change to use arguments
-void setTime(RTC_HandleTypeDef *hrtc) {
+// set rtc time. uses perosnal struct as arg
+void setTime(RTC_HandleTypeDef *hrtc, struct times *t) {
 	RTC_TimeTypeDef stime = {0};	// change to malloc call? does that work in embedded?
 
 	// set using args later
-	stime.Hours = 1;
-	stime.Minutes = 1;
-	stime.Seconds = 1;
+	stime.Hours = t->hr;
+	stime.Minutes = t->min;
+	stime.Seconds = t->sec;
 
 	stime.TimeFormat = RTC_HOURFORMAT_24;
 
@@ -39,53 +33,33 @@ void setTime(RTC_HandleTypeDef *hrtc) {
 	HAL_RTC_SetTime(hrtc, &stime, RTC_FORMAT_BIN);
 }
 
-// change to use args
-void setDateTime(RTC_HandleTypeDef *hrtc) {
-	// ---- time ----
-	RTC_TimeTypeDef stime = {0};
-
-	// set with args later
-	stime.Hours = 2;
-	stime.Minutes = 2;
-	stime.Seconds = 2;
-
-	stime.TimeFormat = RTC_HOURFORMAT_24;
-
-	// not sure what these do
-	stime.SubSeconds = 0;
-	stime.SecondFraction = 0;
-
-	stime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;		// add daylight savings later?
-	stime.StoreOperation = RTC_STOREOPERATION_SET;		// not sure what this does
-	// ---- end time ----
-
+// set rtc date. uses personal struct
+void setDate(RTC_HandleTypeDef *hrtc, struct dates *d) {
 	// ---- date ----
 	RTC_DateTypeDef sdate = {0};
 
-	// set with args later
-	sdate.Month = RTC_MONTH_NOVEMBER;
-	sdate.Date = 8;
-	sdate.WeekDay = RTC_WEEKDAY_FRIDAY;
-	sdate.Year = 19; 		// set only between 0-99. part of the library (!?)
-	// ---- end date ----
+	sdate.Month = d->month;
+	sdate.Date = d->date;
+	sdate.WeekDay = d->weekday;
+	sdate.Year = d->yr % 100; 		// set only between 0-99. part of the library (!?)
 
-	// do nothing until done
-//	while (HAL_RTC_SetTime(hrtc, &stime, RTC_FORMAT_BIN) != HAL_OK);
-//	while (HAL_RTC_SetDate(hrtc, &sdate, RTC_FORMAT_BIN) != HAL_OK);
-
-	HAL_RTC_SetTime(hrtc, &stime, RTC_FORMAT_BIN);
 	HAL_RTC_SetDate(hrtc, &sdate, RTC_FORMAT_BIN);
 }
 
+void setDateTime(RTC_HandleTypeDef *hrtc, struct dates *d, struct times *t) {
+	setDate(hrtc, d);
+	setTime(hrtc, t);
+}
+
 // for time of day+week
-void setAlarm(RTC_HandleTypeDef *hrtc) {
+void setAlarm(RTC_HandleTypeDef *hrtc, struct alarmTimes *a) {
 	RTC_AlarmTypeDef salarm = {0};
 	RTC_TimeTypeDef salarmtime = {0};
 
 	// change to set with args
-	salarmtime.Hours = 0;
-	salarmtime.Minutes = 0;
-	salarmtime.Seconds = 0;
+	salarmtime.Hours = a->hr;
+	salarmtime.Minutes = a->min;
+	salarmtime.Seconds = a->sec;
 	salarmtime.TimeFormat = RTC_HOURFORMAT_24;
 	salarmtime.SubSeconds = 0;
 	salarmtime.SecondFraction = 0;
@@ -96,8 +70,8 @@ void setAlarm(RTC_HandleTypeDef *hrtc) {
 	salarm.AlarmMask = RTC_ALARMMASK_ALL;
 	salarm.AlarmSubSecondMask = RTC_ALARMSUBSECONDMASK_ALL;
 	salarm.AlarmDateWeekDaySel = RTC_ALARMDATEWEEKDAYSEL_WEEKDAY;
-	salarm.AlarmDateWeekDay = RTC_WEEKDAY_MONDAY;
-	salarm.Alarm = RTC_ALARM_B;
+	salarm.AlarmDateWeekDay = a->weekday;
+	salarm.Alarm = RTC_ALARM_A;			// change if using different alarm
 
 	// do nothing until done
 	while (HAL_RTC_SetAlarm_IT(hrtc, &salarm, RTC_FORMAT_BCD) != HAL_OK);
@@ -150,9 +124,62 @@ void setAlarm(RTC_HandleTypeDef *hrtc) {
 // check rtc exported macros section
 // get/clear flag macro (difference between exti flag macros?)
 //   bunch of flag definitions for different flags :O
-//void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtc) {}
+void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtc) {
+	// change pin to whatever's accessible
+	// using PC0
+	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
+}
 
-//void HAL_RTC_AlarmBEventCallback(RTC_HandleTypeDef *hrtc) {}
+// ---- clock get functions ----
+
+// maybe needs subseconds?
+void getTime(RTC_HandleTypeDef *hrtc, struct times *t) {
+	RTC_TimeTypeDef stime;
+
+	// programming manual says to read time after date. something shadow registers.
+	// not done automatically in HAL
+	HAL_RTC_GetTime(hrtc, &stime, RTC_FORMAT_BIN);
+	HAL_RTC_GetDate(hrtc, NULL, RTC_FORMAT_BIN);
+
+	t->hr = stime.Hours;
+	t->min = stime.Minutes;
+	t->sec = stime.Seconds;
+}
+
+void getDate(RTC_HandleTypeDef *hrtc, struct dates *d) {
+	RTC_DateTypeDef sdate;
+
+	// programming manual says to read time after date. something shadow registers.
+	// not done automatically in HAL
+	HAL_RTC_GetTime(hrtc, NULL, RTC_FORMAT_BIN);
+	HAL_RTC_GetDate(hrtc, &sdate, RTC_FORMAT_BIN);
+
+	d->yr = sdate.Year > 50 ? sdate.Year+1900 : sdate.Year+2000;
+	d->month = sdate.Month;
+	d->date = sdate.Date;
+	d->weekday = sdate.WeekDay;
+}
+
+// not using getDate and getTime for efficiency (?)
+void getDateTime(RTC_HandleTypeDef *hrtc, struct dates *d, struct times *t) {
+	RTC_DateTypeDef sdate;
+	RTC_TimeTypeDef stime;
+
+	// programming manual says to read time after date. something shadow registers.
+	// not done automatically in HAL
+	HAL_RTC_GetTime(hrtc, &stime, RTC_FORMAT_BIN);
+	HAL_RTC_GetDate(hrtc, &sdate, RTC_FORMAT_BIN);
+
+	d->yr = sdate.Year > 50 ? sdate.Year+1900 : sdate.Year+2000;
+	d->month = sdate.Month;
+	d->date = sdate.Date;
+	d->weekday = sdate.WeekDay;
+
+	t->hr = stime.Hours;
+	t->min = stime.Minutes;
+	t->sec = stime.Seconds;
+}
+// ---- end of clock get functions ----
 
 // ---- clock print functions ----
 // print functions for RTC
@@ -161,37 +188,31 @@ void setAlarm(RTC_HandleTypeDef *hrtc) {
 void printTime(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi) {
 	char str[40];		// problems when using only char*
 
-	RTC_TimeTypeDef stime;		// problems when unitialized? doesn't look like it so far
-	HAL_RTC_GetTime(hrtc, &stime, RTC_FORMAT_BIN);
-	HAL_RTC_GetDate(hrtc, NULL, RTC_FORMAT_BIN);
+	struct times t;
+	getTime(hrtc, &t);
 
-	uint8_t sec = stime.Seconds;
-	uint8_t min = stime.Minutes;
-	uint8_t hr = stime.Hours;
-	sprintf(str, "sec: %2d", sec);
+	sprintf(str, "sec: %2d", t.sec);
 	drawText(0, 0, 1, ST77XX_BLACK, ST77XX_WHITE, str, hspi);
-	sprintf(str, "min: %2d", min);
+	sprintf(str, "min: %2d", t.min);
 	drawText(0, 10, 1, ST77XX_BLACK, ST77XX_WHITE, str, hspi);
-	sprintf(str, "hr: %3d", hr);
+	sprintf(str, "hr: %3d", t.hr);
 	drawText(0, 20, 1, ST77XX_BLACK, ST77XX_WHITE, str, hspi);
 }
 
 void printDate(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi) {
 	char str[40];		// problems when using only char*
 
-	RTC_DateTypeDef sdate;		// problems when unitialized? doesn't look like it so far
-	HAL_RTC_GetTime(hrtc, NULL, RTC_FORMAT_BIN);
-	HAL_RTC_GetDate(hrtc, &sdate, RTC_FORMAT_BIN);
+	struct dates d;
+	getDate(hrtc, &d);
 
-	uint8_t yr = sdate.Year;
-	uint8_t month = sdate.Month;
-	uint8_t day = sdate.Date;
-	sprintf(str, "year: %3d", yr);
+	sprintf(str, "year: %3d", d.yr);
 	drawText(0, 40, 1, ST77XX_BLACK, ST77XX_WHITE, str, hspi);
-	sprintf(str, "month: %4d", month);
+	sprintf(str, "month: %4d", d.month);
 	drawText(0, 50, 1, ST77XX_BLACK, ST77XX_WHITE, str, hspi);
-	sprintf(str, "day: %2d", day);
+	sprintf(str, "day: %2d", d.date);
 	drawText(0, 60, 1, ST77XX_BLACK, ST77XX_WHITE, str, hspi);
+	sprintf(str, "day: %2d", d.weekday);	// probably not gonna show nice since its an enum
+	drawText(0, 70, 1, ST77XX_BLACK, ST77XX_WHITE, str, hspi);
 }
 
 void printDateTime(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi) {
@@ -202,20 +223,36 @@ void printDateTime(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi) {
 
 // tests clock functions. assumes SPI display using TFT_display.c is available
 void clockTest(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi) {
+	struct times t = {1, 1, 1};
+	struct dates d = {19, 11, 13};
+
 	HAL_Delay(1000);
 	printDateTime(hrtc, hspi);
 	HAL_Delay(2000);
 	printDateTime(hrtc, hspi);
 
 	HAL_Delay(1000);
-	setTime(hrtc);
+	setTime(hrtc, &t);
 	printDateTime(hrtc, hspi);
 	HAL_Delay(1000);
 	printDateTime(hrtc, hspi);
 
 	HAL_Delay(1000);
-	setDateTime(hrtc);
+	setDateTime(hrtc, &d, &t);
 	printDateTime(hrtc, hspi);
 	HAL_Delay(1000);
 	printDateTime(hrtc, hspi);
+}
+
+void alarmTest(RTC_HandleTypeDef *hrtc, SPI_HandleTypeDef *hspi) {
+	struct dates d;
+	struct times t;
+
+	getDateTime(hrtc, &d, &t);
+
+	if (t.sec > 60) t.min += 1;
+	t.sec = (t.sec+10) % 60;
+	struct alarmTimes a = {t.hr, t.min, t.sec, d.weekday};
+
+	setAlarm(hrtc, &a);
 }
